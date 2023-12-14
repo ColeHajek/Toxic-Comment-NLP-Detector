4a331ba94da18f92e1c635819f7f05b2a73cc1af
jjberk1234 <jjberkel.8@gmail.com>
2023-12-14 15:19:52 -0800

    LSTM Dense

--- LSTM_Dense.py
+++ LSTM_Dense.py
@@ -1,21 +1,34 @@
-#import torch
-#from torch import nn
-#from torch.utils.data import DataLoader
 import tools
-import tensorflow as tf
-from keras.preprocessing.text import Tokenizer
-from keras.models import Sequential
-from keras.layers import LSTM,Embedding,Dropout,Flatten,Dense,GlobalAveragePooling1D,SpatialDropout1D
-from keras.metrics import BinaryAccuracy, Precision, Recall
-import numpy as np
 import os
-import pandas as pd
+import sys
 
-dataset_path = "C:\\Users\\blues\\OneDrive\\Desktop\\UCI\\Fall '23\\CS 178\\Final Project\\data\\train.csv"
+from keras.models import Sequential
+from keras.layers import LSTM,Embedding,Dense,SpatialDropout1D
+from keras.metrics import BinaryAccuracy, Precision, Recall
+from keras.utils import set_random_seed
+
+
+
+seed = 42
+set_random_seed(seed)
+
+# Load in training data
+dataset_path = os.path.join(os.getcwd(), 'data\\50pruned_w_gen.csv')
 Xtr,ytr,Xte,yte = tools.preprocess_data(dataset_path)
 
-# Define model
+# Path to save model weights and model output
+checkpoint_path = os.path.join(os.getcwd(), 'models\\LSTM_Dense_50_w_gen.ckpt')
+output_path = os.path.join(os.getcwd(),'results\\50_w_gen_results.txt')
 
+
+# True if loading model weights from file
+# False if training model from scratch
+load_model = True
+
+# Set True to create file of model weights
+save_model = False
+
+# Model definition
 num_words = 1000
 embedding_dim = 100
 drop_value = 0.2
@@ -27,35 +40,26 @@ model.add(SpatialDropout1D(0.2))
 model.add(LSTM(10,dropout=drop_value,recurrent_dropout=drop_value))
 model.add(Dense(6,activation='sigmoid'))
 
-model.compile(loss='categorical_crossentropy',
-            optimizer='adam',
-            metrics=[BinaryAccuracy(),
-                     Precision(),
-                     Recall()])
-model.summary()
+with open(output_path,'w') as f:
+    sys.stdout = f
 
-checkpoint_path = "C:\\Users\\blues\\OneDrive\\Desktop\\UCI\\Fall '23\\CS 178\\Final Project\\models\\LSTM_Dense_training_cleaned.ckpt"
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
+                metrics=[BinaryAccuracy(),
+                        Precision(),
+                        Recall()])
+    model.summary()
 
-# True if loading model weights from file
-# False if trainign model from scratch
-load_model = False
+    if load_model:
+        model.load_weights(checkpoint_path)
+    else:
+        # Training
+        num_epochs = 1
+        history = model.fit(Xtr,ytr,epochs=num_epochs,
+                validation_data=(Xte,yte),verbose=2)
 
-# Set True to create file of model weights
-save_model = True
+        if save_model ==True:
+            model.save_weights(checkpoint_path)
 
-if load_model==False:
-    # Training
-    num_epochs = 1
-    history = model.fit(Xtr,ytr,epochs=num_epochs,
-            validation_data=(Xte,yte),verbose=2)
+    sys.stdout = sys.__stdout__
 
-    if save_model ==True:
-        model.save_weights(checkpoint_path)
-
-if load_model == True:
-    model.load_weights(checkpoint_path)
-
-train_results = model.evaluate(Xtr,ytr)
-test_results = model.evaluate(Xte, yte)
-print(f'Train accuracy: {train_results[1]:10.2f}')
-print(f'Test accuracy: {test_results[1]:10.2f}')
