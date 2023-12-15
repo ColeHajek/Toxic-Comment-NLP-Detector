f5cfd371ebcc3f45b924e7eca5674a3bfa23c2be
jjberk1234 <jjberkel.8@gmail.com>
2023-12-14 16:30:48 -0800

    Prevents comment text from being interpreted as non-string

--- tools.py
+++ tools.py
@@ -11,27 +11,24 @@ from keras.utils import pad_sequences
 
 from sklearn.model_selection import train_test_split
 
-
-
 def preprocess_data(file_path):
     #get data from csv files
-    data = pd.read_csv(file_path)
-    # Extract features and labels
-    tokenizer = Tokenizer(num_words=500,lower=True)
+    data = pd.read_csv(file_path,dtype={'comment_text':'string'})
+    
+    # Extract features
+    tokenizer = Tokenizer(num_words=500)
     tokenizer.fit_on_texts(data['comment_text'].values)
     X = tokenizer.texts_to_sequences(data['comment_text'].values)
     X = pad_sequences(X,maxlen=250)
 
-
+    # Extract labels
     Y = pd.get_dummies(data[['toxic', 'severe_toxic',
                                  'obscene', 'threat', 'insult',
                                  'identity_hate']]).values
 
-
+    # Split data
     Xtr,Xte,ytr,yte = train_test_split(X,Y, test_size=0.2)
-
-   
-    return Xtr,ytr,Xte,yte
+    return Xtr,Xte,ytr,yte
 
 def tokenize(x,num_words=100):
     tokenizer =Tokenizer(num_words=num_words,lower=False)
