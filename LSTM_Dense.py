2585b1afde62376a285638e1c524842e7dc7cd7e
jjberk1234 <jjberkel.8@gmail.com>
2023-12-14 17:37:21 -0800

    Working LSTM

--- LSTM_Dense.py
+++ LSTM_Dense.py
@@ -10,56 +10,72 @@ from keras.utils import set_random_seed
 seed = 42
 set_random_seed(seed)
 
-# Load in training data
-train_data_fn = 'NAME OF YOUR TRAINING DATA .csv FILE'
-train_data_path = os.path.join(os.getcwd(), train_data_fn)
-Xtr,Xte,ytr,yte = tools.preprocess_data(train_data_path)
 
-# Path to save model weights and model output
-checkpoint_fn = 'NAME OF YOUR CHECKPOINT .ckpt FILE'
-results_fn = 'NAME OF YOUR RESULTS .txt FILE'
-checkpoint_path = os.path.join(os.getcwd(), checkpoint_fn)
-output_path = os.path.join(os.getcwd(), results_fn)
+def run_model(Xtr,ytr,Xte,yte,load_model=False):
+    # Model definition
+    num_words = 1000
+    embedding_dim = 100
+    drop_value = 0.2
 
-# True if loading model weights from file
-# False if training model from scratch
-load_model = True
+    model = Sequential()
+    model.add(Embedding(num_words,embedding_dim,input_length=Xtr.shape[1]))
+    model.add(SpatialDropout1D(0.2))
+    model.add(LSTM(10,dropout=drop_value,recurrent_dropout=drop_value))
+    model.add(Dense(6,activation='sigmoid'))
 
-# Set True to create file of model weights
-save_model = False
+    with open(output_path,'w') as f:
+        sys.stdout = f
 
-# Model definition
-num_words = 1000
-embedding_dim = 100
-drop_value = 0.2
-max_len = 500
+        model.compile(loss='categorical_crossentropy',
+                    optimizer='adam',
+                    metrics=[BinaryAccuracy(),
+                            Precision(),Recall()])
+        model.summary()
 
-model = Sequential()
-model.add(Embedding(num_words,embedding_dim,input_length=Xtr.shape[1]))
-model.add(SpatialDropout1D(0.2))
-model.add(LSTM(10,dropout=drop_value,recurrent_dropout=drop_value))
-model.add(Dense(6,activation='sigmoid'))
+        if load_model:
+            model.load_weights(checkpoint_path)
+        else:
+            # Training
+            num_epochs = 1
+            history = model.fit(Xtr,ytr,epochs=num_epochs,
+                    validation_data=(Xte,yte),verbose=2)
 
-with open(output_path,'w') as f:
-    sys.stdout = f
-
-    model.compile(loss='categorical_crossentropy',
-                optimizer='adam',
-                metrics=[BinaryAccuracy(),
-                        Precision(),
-                        Recall()])
-    model.summary()
-
-    if load_model:
-        model.load_weights(checkpoint_path)
-    else:
-        # Training
-        num_epochs = 1
-        history = model.fit(Xtr,ytr,epochs=num_epochs,
-                validation_data=(Xte,yte),verbose=2)
-
-        if save_model ==True:
+            # Save weights
             model.save_weights(checkpoint_path)
 
-    sys.stdout = sys.__stdout__
+        sys.stdout = sys.__stdout__
+
+    return history
+
+train_data_fns = ['data/50pruned_w_gen.csv','data/60pruned_w_gen.csv',
+                     'data/70pruned_w_gen.csv','data/80pruned_w_gen.csv',
+                     'data/90pruned_w_gen.csv']
+
+test_data_fn = 'data/testclean.csv'
+
+checkpoint_fns = ['model/50pruned_w_gen.ckpt','model/60pruned_w_gen.ckpt',
+                     'model/70pruned_w_gen.ckpt','model/80pruned_w_gen.ckpt',
+                     'model/90pruned_w_gen.ckpt']
+
+results_fns = ['results/50pruned_w_gen.txt','results/60pruned_w_gen.txt',
+               'results/70pruned_w_gen.txt','results/80pruned_w_gen.txt',
+               'results/90pruned_w_gen.txt']
+
+for i in range(3,5):
+    # Load in training data
+    train_data_path = os.path.join(os.getcwd(), train_data_fns[i])
+    Xtr,ytr = tools.preprocess_data(train_data_path)
+
+    # Load in test data
+    test_data_path = os.path.join(os.getcwd(), test_data_fn)
+    Xte,yte = tools.preprocess_data(test_data_path)
+
+    # Path to save/load model weights and model output
+    checkpoint_path = os.path.join(os.getcwd(), checkpoint_fns[i])
+    output_path = os.path.join(os.getcwd(), results_fns[i])
+
+    # Run model
+    run_model(Xtr,ytr,Xte,yte,False)
+
+
 
